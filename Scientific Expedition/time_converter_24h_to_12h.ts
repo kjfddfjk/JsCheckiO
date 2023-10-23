edd054e712678c20df2ec416aaf955037958bdb5
Kenny Wu <kjfddfjk@yahoo.com.hk>
2023-10-23 11:29:50 +0800

    finish time-converter-24h-to-12h

--- Scientific Expedition/time_converter_24h_to_12h.ts
+++ Scientific Expedition/time_converter_24h_to_12h.ts
@@ -19,19 +19,17 @@
 
 "use strict";
 
-function timeConverter(dayTime) {
-    // your code here
-    return 0;
+function timeConverter(dayTime: string): string {
+    let date : Date = new Date(0, 0, 0, Number(`${dayTime.slice(0, 2)}`), Number(`${dayTime.slice(3, 5)}`));
+    return date.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", hour12: true }).replace(/^0/,"").replace("AM", "a.m.").replace("PM", "p.m.");
 }
 
 var assert = require('assert');
-if (!global.is_checking) {
-    console.log('Example:')
-    console.log(timeConverter('12:30'))
+console.log('Example:')
+console.log(timeConverter('12:30'))
 
-    // These "asserts" are used for self-checking and not for an auto-testing
-    assert.equal(timeConverter('12:30'), '12:30 p.m.')
-    assert.equal(timeConverter('09:00'), '9:00 a.m.')
-    assert.equal(timeConverter('23:15'), '11:15 p.m.')
-    console.log("Coding complete? Click 'Check' to earn cool rewards!");
-}
+// These "asserts" are used for self-checking and not for an auto-testing
+assert.equal(timeConverter('12:30'), '12:30 p.m.')
+assert.equal(timeConverter('09:00'), '9:00 a.m.')
+assert.equal(timeConverter('23:15'), '11:15 p.m.')
+console.log("Coding complete? Click 'Check' to earn cool rewards!");
